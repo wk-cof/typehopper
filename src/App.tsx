bacef638d81051981828995e1d7d922b542edd53
Mikhail Gorbachev <gorbachevma@gmail.com>
2023-04-28 00:56:04 -0400

    Added prettier autoformatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import Phaser from 'phaser';
+
+import { HelloWorldScene } from './scenes/HelloWorldScene';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  pysics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+};
+
+const dumbstring = 'asdf';
+
 function App() {
   return (
     <>
-      <div>
-        Hello world
-      </div>
+      <div>Hello world</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
