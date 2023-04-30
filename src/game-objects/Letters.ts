c7c630236ea93fca5cccf95c33ea4800576d4d59
Mikhail Gorbachev <gorbachevma@gmail.com>
2023-04-29 20:06:26 -0400

    added letters class

---
+++ src/game-objects/Letters.ts
@@ -0,0 +1,48 @@
+import Phaser from 'phaser';
+import Letter from './Letter';
+
+export default class Letters {
+  private scene: Phaser.Scene;
+  private letterSpeed: number;
+  private letters: Array<Letter>;
+
+  constructor(scene: Phaser.Scene, letterSpeed: number) {
+    this.scene = scene;
+    this.letterSpeed = letterSpeed;
+    this.letters = [];
+  }
+
+  createInitialLetters(): void {
+    for (let i = 0; i < 4; i++) {
+      this.createNewLetter(500 + i * 400);
+    }
+  }
+
+  createNewLetter(x: number): void {
+    const letters = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
+    const randomLetter = letters.charAt(Math.floor(Math.random() * letters.length));
+    const xPos = x;
+    const newLetter = new Letter(this.scene, randomLetter, xPos, 300, this.letterSpeed);
+    this.letters.push(newLetter);
+  }
+
+  update(time: number, delta: number): void {
+    this.letters.forEach((letter) => {
+      letter.update(time, delta);
+    });
+  }
+
+  getFirstLetter(): Letter {
+    return this.letters[0];
+  }
+
+  removeFirstLetter(): void {
+    const removedLetter = this.letters.shift();
+    removedLetter?.destroy();
+  }
+
+  addNewLetterAfterLast(): void {
+    const lastLetter = this.letters[this.letters.length - 1].getGameObject().x;
+    this.createNewLetter(lastLetter + 400);
+  }
+}
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,23 +1,13 @@
-// GameScene.ts
 import Phaser from 'phaser';
 import Bunny from '../game-objects/Bunny';
 import Letter from '../game-objects/Letter';
 import Background from '../game-objects/Background';
-
-type KeyMaps = {
-  [key: string]: {
-    emitOnRepeat:boolean;
-    enabled: boolean;
-    isDown: boolean
-    isUp: boolean;
-    keyCode: number;
-  };
-};
+import Letters from '../game-objects/Letters';
 
 export default class GameScene extends Phaser.Scene {
   private bunny!: Bunny;
   private background!: Background;
-  private letters!: Array<Letter>;
+  private letters!: Letters;
   private letterPressed: string | undefined;
   private letterSpeed = 150;
   constructor() {
@@ -36,34 +26,30 @@ export default class GameScene extends Phaser.Scene {
     this.background.create();
     this.bunny.create();
 
-    this.letters = [];
+    this.letters = new Letters(this, this.letterSpeed);
+    this.letters.createInitialLetters();
+
     this.input.keyboard?.on('keyup', (event: any) => {
       this.letterPressed = event.key;
     });
-
-    // Create initial letters
-    for (let i = 0; i < 4; i++) {
-      this.createNewLetter(500 + i * 400);
-    }
   }
 
   update(time: number, delta: number): void {
-    if (this.letterPressed && this.letterPressed.toLowerCase() === this.letters[0].letter.toLowerCase()) {
-      const removedLetter = this.letters.shift();
-      removedLetter?.destroy();
-      this.createNewLetter(this.letters[this.letters.length - 1].getGameObject().x + 400);
+    const reachedLetter = this.inJumpDistance(this.letters.getFirstLetter().getGameObject());
+    if (!reachedLetter) {
+      this.background.update(delta);
+      this.letters.update(time, delta);
+      return;
+    }
+
+    if (this.letterPressed && this.letterPressed.toLowerCase() === this.letters.getFirstLetter().letter.toLowerCase()) {
+      this.letters.removeFirstLetter();
+      this.letters.addNewLetterAfterLast();
       this.bunny.hop();
       this.letterPressed = undefined;
     }
     this.bunny.update(time, delta);
 
-    const reachedLetter = this.inJumpDistance(this.letters[0].getGameObject());
-    if (!reachedLetter) {
-      this.background.update(delta);
-      this.letters.forEach((letter) => {
-        letter.update(time, delta);
-      });
-    }
   }
 
   inJumpDistance(letter: Phaser.GameObjects.Text): boolean {
@@ -74,14 +60,7 @@ export default class GameScene extends Phaser.Scene {
       letter.y
     );
 
-    return distance < 50;
-  }
-
-  createNewLetter(x?: number): void {
-    const letters = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
-    const randomLetter = letters.charAt(Math.floor(Math.random() * letters.length));
-    const xPos = x || 1500;
-    const newLetter = new Letter(this, randomLetter, xPos, 300, this.letterSpeed);
-    this.letters.push(newLetter);
+    return distance < 100;
   }
 }
+
