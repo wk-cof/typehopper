d1ddc24ef1e6eaa9c607a106b01b37f0c1e2fc06
Mikhail Gorbachev <gorbachevma@gmail.com>
2023-05-02 21:25:17 -0400

    new level

---
+++ scripts/resize.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import os
+
+def resize_images(image_list, width, height, output_folder):
+    for image_path in image_list:
+        img = Image.open(image_path)
+        resized_img = img.resize((width, height), Image.ANTIALIAS)
+        base_name = os.path.basename(image_path)
+        output_path = os.path.join(output_folder, base_name)
+        resized_img.save(output_path)
+        print(f"Resized image saved to {output_path}")
+
+def main():
+    image_list = [
+      'public/assets/Cartoon_Forest_BG_01/Layers/Sky.png',
+      'public/assets/Cartoon_Forest_BG_01/Layers/BG_Decor.png',
+      'public/assets/Cartoon_Forest_BG_01/Layers/Middle_Decor.png',
+      'public/assets/Cartoon_Forest_BG_01/Layers/Foreground.png',
+      'public/assets/Cartoon_Forest_BG_01/Layers/Ground.png'
+    ]
+
+    width = 785 #int(input("Enter the width: "))
+    height = 384 #int(input("Enter the height: "))
+    output_folder = input("Enter the output folder: ")
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    resize_images(image_list, width, height, output_folder)
+
+if __name__ == "__main__":
+    main()
--- src/game-objects/Background.ts
+++ src/game-objects/Background.ts
@@ -2,47 +2,105 @@
 import Phaser from 'phaser';
 import { LEVEL_DIMENSIONS } from '../game';
 
-const BACKGROUND_KEY = 'background';
+const SKY_KEY = 'sky';
+const BG_DECOR_KEY = 'bg_decor';
+const MIDDLE_DECOR_KEY = 'middle_decor';
+const FOREGROUND_KEY = 'foreground';
+const GROUND_KEY = 'ground';
 
 export default class Background {
   private scene: Phaser.Scene;
-  private background1!: Phaser.GameObjects.Image;
-  private background2!: Phaser.GameObjects.Image;
   private scrollSpeed: number;
+  private layers: {
+    key: string;
+    image1: Phaser.GameObjects.Image;
+    image2: Phaser.GameObjects.Image;
+    speed: number;
+  }[];
 
   constructor(scene: Phaser.Scene, scrollSpeed: number) {
     this.scene = scene;
     this.scrollSpeed = scrollSpeed;
+    this.layers = [];
   }
 
   preload() {
-    // this.scene.load.image(BACKGROUND_KEY, 'assets/background.jpg');
-    this.scene.load.image(BACKGROUND_KEY, 'assets/level-2.png');
+    this.scene.load.image(
+      SKY_KEY,
+      'assets/Cartoon_Forest_BG_01/Layers/Sky.png'
+    );
+    this.scene.load.image(
+      BG_DECOR_KEY,
+      'assets/Cartoon_Forest_BG_01/Layers/BG_Decor.png'
+    );
+    this.scene.load.image(
+      MIDDLE_DECOR_KEY,
+      'assets/Cartoon_Forest_BG_01/Layers/Middle_Decor.png'
+    );
+    this.scene.load.image(
+      FOREGROUND_KEY,
+      'assets/Cartoon_Forest_BG_01/Layers/Foreground.png'
+    );
+    this.scene.load.image(
+      GROUND_KEY,
+      'assets/Cartoon_Forest_BG_01/Layers/Ground.png'
+    );
   }
 
   create() {
-    this.background1 = this.scene.add.image(
-      LEVEL_DIMENSIONS.x / 2,
-      LEVEL_DIMENSIONS.y / 2,
-      BACKGROUND_KEY
-    );
-    this.background2 = this.scene.add.image(
-      LEVEL_DIMENSIONS.x / 2 + LEVEL_DIMENSIONS.x,
-      LEVEL_DIMENSIONS.y / 2,
-      BACKGROUND_KEY
-    );
+    this.layers = [
+      {
+        key: SKY_KEY,
+        image1: this.createImage(SKY_KEY, LEVEL_DIMENSIONS.x / 2),
+        image2: this.createImage(SKY_KEY, LEVEL_DIMENSIONS.x * 1.5),
+        speed: 0.1,
+      },
+      {
+        key: BG_DECOR_KEY,
+        image1: this.createImage(BG_DECOR_KEY, LEVEL_DIMENSIONS.x / 2),
+        image2: this.createImage(BG_DECOR_KEY, LEVEL_DIMENSIONS.x * 1.5),
+        speed: 0.3,
+      },
+      {
+        key: MIDDLE_DECOR_KEY,
+        image1: this.createImage(MIDDLE_DECOR_KEY, LEVEL_DIMENSIONS.x / 2),
+        image2: this.createImage(MIDDLE_DECOR_KEY, LEVEL_DIMENSIONS.x * 1.5),
+        speed: 0.5,
+      },
+      {
+        key: FOREGROUND_KEY,
+        image1: this.createImage(FOREGROUND_KEY, LEVEL_DIMENSIONS.x / 2),
+        image2: this.createImage(FOREGROUND_KEY, LEVEL_DIMENSIONS.x * 1.5),
+        speed: 0.7,
+      },
+      {
+        key: GROUND_KEY,
+        image1: this.createImage(GROUND_KEY, LEVEL_DIMENSIONS.x / 2),
+        image2: this.createImage(GROUND_KEY, LEVEL_DIMENSIONS.x * 1.5),
+        speed: 1.0,
+      },
+    ];
+  }
+
+  private createImage(key: string, x: number): Phaser.GameObjects.Image {
+    const image = this.scene.add
+      .image(x, LEVEL_DIMENSIONS.y / 2, key)
+      .setOrigin(0.5, 0.5);
+    // image.setScale(LEVEL_DIMENSIONS.x / image.width);
+    return image;
   }
 
   update(delta: number) {
-    this.background1.x -= (this.scrollSpeed * delta) / 1000;
-    this.background2.x -= (this.scrollSpeed * delta) / 1000;
+    this.layers.forEach(layer => {
+      layer.image1.x -= (this.scrollSpeed * layer.speed * delta) / 1000;
+      layer.image2.x -= (this.scrollSpeed * layer.speed * delta) / 1000;
+      if (layer.image1.x < -LEVEL_DIMENSIONS.x / 2) {
+        layer.image1.x = LEVEL_DIMENSIONS.x / 2 + LEVEL_DIMENSIONS.x;
+      }
 
-    if (this.background1.x < -LEVEL_DIMENSIONS.x / 2) {
-      this.background1.x = LEVEL_DIMENSIONS.x / 2 + LEVEL_DIMENSIONS.x;
-    }
-
-    if (this.background2.x < -LEVEL_DIMENSIONS.x / 2) {
-      this.background2.x = LEVEL_DIMENSIONS.x / 2 + LEVEL_DIMENSIONS.x;
-    }
+      if (layer.image2.x < -LEVEL_DIMENSIONS.x / 2) {
+        layer.image2.x = LEVEL_DIMENSIONS.x / 2 + LEVEL_DIMENSIONS.x;
+      }
+    });
   }
 }
